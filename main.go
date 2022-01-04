f4548512e861da21e9bbdb481b2d3627ebd8cc9a
beepboop-lettuce <ericaharris0486@gmail.com>
2022-01-04 15:15:38 -0600

    String interpolation

---
+++ main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+var reader *bufio.Reader
+
+func main() {
+	reader = bufio.NewReader(os.Stdin)
+	userName := readString("What is your name?")
+
+	age := readInt("How old are you?")
+
+	//fmt.Println("Your name is " + userName + ". You are", age, "years old.")
+
+	//fmt.Println(fmt.Sprintf("Your name is %s. You are %d years old.", userName, age))
+	fmt.Sprintf("Your name is %s. You are %d years old.\n", userName, age)
+
+}
+
+func prompt() {
+	fmt.Print("-> ")
+}
+
+func readString(s string) string {
+	for {
+		fmt.Println(s)
+		prompt()
+
+		userInput, _ := reader.ReadString('\n')
+		userInput = strings.Replace(userInput, "\r\n", "", -1)
+		userInput = strings.Replace(userInput, "\n", "", -1)
+		if userInput == "" {
+			fmt.Println("Please enter a value")
+		} else {
+			return userInput
+		}
+	}
+}
+
+func readInt(s string) int {
+	for {
+		fmt.Println(s)
+		prompt()
+
+		userInput, _ := reader.ReadString('\n')
+		userInput = strings.Replace(userInput, "\r\n", "", -1)
+		userInput = strings.Replace(userInput, "\n", "", -1)
+
+		num, err := strconv.Atoi(userInput)
+		if err != nil {
+			fmt.Println("Please enter a whole number")
+		} else {
+			return num
+		}
+	}
+}

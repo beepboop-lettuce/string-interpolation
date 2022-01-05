5e676cb1b2af24d9c4e5be8e594ec85d2a69019c
beepboop-lettuce <ericaharris0486@gmail.com>
2022-01-05 11:30:51 -0600

    function for reading bool

--- main.go
+++ main.go
@@ -6,20 +6,36 @@ import (
 	"os"
 	"strconv"
 	"strings"
+
+	"log"
+
+	"github.com/eiannone/keyboard"
 )
 
 var reader *bufio.Reader
 
+type User struct {
+	UserName        string
+	Age             int
+	FavouriteNumber float64
+	OwnsADog        bool
+}
+
 func main() {
 	reader = bufio.NewReader(os.Stdin)
-	userName := readString("What is your name?")
 
-	age := readInt("How old are you?")
+	var user User
 
-	//fmt.Println("Your name is " + userName + ". You are", age, "years old.")
+	user.UserName = readString("What is your name?")
+	user.Age = readInt("How old are you?")
+	user.FavouriteNumber = readFloat("What is your favourite number?")
+	user.OwnsADog = readBool("Do you own a dog? Press y for yes or n for no.")
 
-	//fmt.Println(fmt.Sprintf("Your name is %s. You are %d years old.", userName, age))
-	fmt.Sprintf("Your name is %s. You are %d years old.\n", userName, age)
+	fmt.Printf("Your name is %s. You are %d years old. Your favourite number is %.2f. You own a dog: %t.\n",
+		user.UserName,
+		user.Age,
+		user.FavouriteNumber,
+		user.OwnsADog)
 
 }
 
@@ -57,6 +73,50 @@ func readInt(s string) int {
 			fmt.Println("Please enter a whole number")
 		} else {
 			return num
+		}
+	}
+}
+
+func readFloat(s string) float64 {
+	for {
+		fmt.Println(s)
+		prompt()
+
+		userInput, _ := reader.ReadString('\n')
+		userInput = strings.Replace(userInput, "\r\n", "", -1)
+		userInput = strings.Replace(userInput, "\n", "", -1)
+
+		num, err := strconv.ParseFloat(userInput, 64)
+		if err != nil {
+			fmt.Println("Please enter a number")
+		} else {
+			return num
+		}
+	}
+}
+
+func readBool(s string) bool {
+	err := keyboard.Open()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer func() {
+		_ = keyboard.Close()
+	}()
+
+	for {
+		fmt.Println(s)
+		char, _, err := keyboard.GetSingleKey()
+		if err != nil {
+			log.Fatal(err)
+		}
+		if strings.ToLower(string(char)) != "y" && strings.ToLower(string(char)) != "n" {
+			fmt.Println("Please type y or n")
+		} else if char == 'n' || char == 'N' {
+			return false
+		} else if char == 'y' || char == 'Y' {
+			return true
 		}
 	}
 }
